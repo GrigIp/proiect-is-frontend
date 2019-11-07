94e9940708f0eb978761cdc0bf55cb1bfdbfcad0
grigIpatiov <ipatiov.grigor@yahoo.com>
2019-11-07 16:16:35 +0200

    Added Api url.

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -113,6 +113,7 @@ module.exports = options => ({
     // drop any unreachable code.
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'development',
+      API_URL: 'http://localhost:8080'
     }),
   ]),
   resolve: {
