1c7d3042169b17eecd4c20bc4f62f7d8cf15fe93
grigIpatiov <ipatiov.grigor@yahoo.com>
2019-11-07 18:38:17 +0200

    Created the NavBar and SignIn components.

---
+++ app/components/NavBar/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    color: '#BB181D'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#BB181D'
+    }
+  }
+});
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              SINU
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </ThemeProvider>
+  );
+}
---
+++ app/containers/SignIn/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import SignInComponent from 'components/SignIn/Loadable';
+
+export default function SignIn() {
+  return <SignInComponent />;
+}
